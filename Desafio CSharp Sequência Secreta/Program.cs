c135969aacd2bcd44dc4d2cac0fb9df99f16bb2b
carlosmg2005 <carlosmg2005@msn.com>
2023-03-26 21:47:09 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio CSharp Sequência Secreta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_CSharp_Sequência_Secreta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanhoSequencia = Convert.ToInt32(Console.ReadLine());
+            int quantidadeNumerosMarcados = 0;
+            int ultimoNumeroLido = 0;
+            for (int i = 0; i < tamanhoSequencia; i++)
+            {
+                int vi = Convert.ToInt32(Console.ReadLine());
+                
+                if (vi != ultimoNumeroLido)
+                ultimoNumeroLido = vi;
+                quantidadeNumerosMarcados++;
+            }
+                Console.WriteLine(quantidadeNumerosMarcados);
+
+            Console.ReadKey();
+        }
+    }
+}
